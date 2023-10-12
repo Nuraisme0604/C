23fd6ab7c17ef493aab10a47fcfd667c2c7f8bc7
Nuraisme0604 <142221839+Nuraisme0604@users.noreply.github.com>
2023-10-12 13:16:45 +0700

    Update test trai tym basic :>.cpp

--- test trai tym basic :>.cpp
+++ test trai tym basic :>.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
-void traitym(int height) {   
+#include <stdlib.h> // cái này cho hàm system("") dùng để xoá màn hình console hoặc là để đổi màu
+#include <unistd.h> // khai báo cho hàm sleep ( nếu dùng)
+void traitym(int height) {   // đệch mợ cái này để viết hàm vẽ trái tym nhá :)))))) 
     for (int i = height / 2; i <= height; i += 2) {
         for (int j = 1; j < height - i; j += 2) {
             printf(" ");
@@ -31,12 +33,14 @@ void traitym(int height) {
 }
 int main(){
 	int x;int k=1;
-
+        system("color C");// biến màu code thành màu light purple
 	while (k=1){
 	printf(" Nhap size cua trai tim:");
 	scanf("%d",&x);
 	traitym(x);
+	getch();
+	system("cls");// cái này dùng để xoá màn hình kiểu sau khi mà vẽ trái tym xong vẽ cái mới nó trở về ban đầu
 	}
-	printf("Xong roi ne !!!! :>");
+	printf("Xong roi ne !!!! :>");// 2 dòng này dư ra nếu muốn nó hoạt động thì chèn thêm cái lệnh k=2; vào trước cái ngoặc ngay ở trên
 	getch();
 }
