9af2b0bcd2b44498240aaef830460747bd4e56a1
Nuraisme0604 <142221839+Nuraisme0604@users.noreply.github.com>
2024-01-03 23:26:08 +0700

    test trai tym basic :>.cpp

--- test trai tym basic :>.cpp
+++ test trai tym basic :>.cpp
@@ -2,39 +2,50 @@
 #include <conio.h>
 #include <stdlib.h> // cái này cho hàm system("") dùng để xoá màn hình console hoặc là để đổi màu
 #include <unistd.h> // khai báo cho hàm sleep ( nếu dùng)
-void traitym(int height) {   // đệch mợ cái này để viết hàm vẽ trái tym nhá :)))))) 
-    for (int i = height / 2; i <= height; i += 2) {
-        for (int j = 1; j < height - i; j += 2) {
-            printf(" ");
-        }
-        for (int j = 1; j <= i; j++) {
-            printf("*");
-        }
+void traitym(int height) // đệch mợ cái này để viết hàm vẽ trái tym nhá :)))))) 
+{ 
+    for (int i = height / 2; i <= height; i += 2) 
+    		{
+        	for (int j = 1; j < height - i; j += 2) 
+			{
+            		printf(" ");
+        		}
+        	for (int j = 1; j <= i; j++) 
+			{
+            		printf("*");
+        		}
 
-        for (int j = 1; j <= height - i; j++) {
-            printf(" ");
-        }
-        for (int j = 1; j <= i; j++) {
-            printf("*");
-        }
+        	for (int j = 1; j <= height - i; j++) 
+			{
+            		printf(" ");
+        		}
+        	for (int j = 1; j <= i; j++) 
+			{
+            		printf("*");
+        		}
  
-        printf("\n");
-    }
+        		printf("\n");
+    		}
 
-    for (int i = height; i >= 1; i--) {
-        for (int j = i; j < height; j++) {
+    for (int i = height; i >= 1; i--) 
+    {
+        for (int j = i; j < height; j++) 
+	{
             printf(" ");
         }
-        for (int j = 1; j <= (i * 2) - 1; j++) {
+        for (int j = 1; j <= (i * 2) - 1; j++) 
+	{
             printf("*");
         }
         printf("\n");
     }
 }
-int main(){
+int main()
+{
 	int x;int k=1;
         system("color C");// biến màu code thành màu light purple
-	while (k=1){
+	while (k=1)
+	{
 	printf(" Nhap size cua trai tim:");
 	scanf("%d",&x);
 	traitym(x);
